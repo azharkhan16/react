6086e390610b2361ac3a17ec9052c2bb3831ce3f
azharkhan16 <azharkhan75447355@gmail.com>
2025-11-05 16:48:19 +0530

    02-componenet Added

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+        <div className='card'>
+          <h1>Azhar Khan</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+  )
+}
+
+export default Card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+            This is navBar
+        
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
